bf9be83d4ceae14e6c851fc7f2b04724b7472ccd
Dylan Moreau <Dyl.zm.95@gmail.com>
2020-04-10 17:00:51 -0400

    Updates

---
+++ precache-manifest.9e362bcd24493457afcabfcc3386293d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a604af0c9de0d714ecaa6cd812bad3d2",
+    "url": "/periodt/index.html"
+  },
+  {
+    "revision": "2da4bddf00e12489d4d5",
+    "url": "/periodt/static/css/main.d73e6a02.chunk.css"
+  },
+  {
+    "revision": "a03e9744d145bd8c24aa",
+    "url": "/periodt/static/js/2.114723ae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/periodt/static/js/2.114723ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2da4bddf00e12489d4d5",
+    "url": "/periodt/static/js/main.963d69da.chunk.js"
+  },
+  {
+    "revision": "ad83e21f04c2fbb7d1d7",
+    "url": "/periodt/static/js/runtime-main.37af3a8f.js"
+  },
+  {
+    "revision": "62fa13c8d6a7597eef48bdd7e57de4a3",
+    "url": "/periodt/static/media/Breaking-Bad.62fa13c8.otf"
+  }
+]);
